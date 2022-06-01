62de21b015d4548e11a5450bdf2c196623bcabf2
Simon Wiles <simonjwiles@gmail.com>
2022-06-01 15:07:40 -0700

    Exempt `neat-csv` from SSR and vite optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
+  vite: {
+    ssr: { external: ["neat-csv"] },
+    optimizeDeps: { exclude: ["neat-csv"] },
+  },
 });
