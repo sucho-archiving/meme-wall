e403558599a18b83b421142626ea6a4912fd53e9
Simon Wiles <simonjwiles@gmail.com>
2022-06-01 16:09:23 -0700

    Update site deploy URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sucho.org/",
+  base: "/meme-gallery/",
+  trailingSlash: "always",
   integrations: [sitemap()],
   vite: {
     ssr: { external: ["neat-csv"] },
