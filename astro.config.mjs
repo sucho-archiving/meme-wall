968a33c81008bc994c9677eceaf6274164c8bab9
Simon Wiles <simonjwiles@gmail.com>
2022-06-07 12:22:55 -0700

    Drop sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://sucho.org/",
   base: "/meme-gallery/",
   trailingSlash: "always",
-  integrations: [sitemap()],
   vite: {
     ssr: { external: ["neat-csv"] },
     optimizeDeps: { exclude: ["neat-csv"] },
