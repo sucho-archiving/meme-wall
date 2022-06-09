d60535179afa90767890ed850c8b7fbca94755cd
Simon Wiles <simonjwiles@gmail.com>
2022-06-09 16:04:28 -0700

    Update to use the live sheet

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Google Drive ID for the spreadsheet
 export const formResponsesSheetId =
-  "12K7vdLa1PFFcuzQ5VWFxzhiJWp7wOqzhYWx-o9Atjps";
+  "1KUOXe5SMMaghl_JI_eZghHG18CXok3D6yb1_SHJM0no";
 
 // location of the local folder for copies of the uploaded media
 export const memeMediaFolder = "meme_media";
