727b0e0f45b43c9f55af95ea7392e3d96b49388b
Simon Wiles <simonjwiles@gmail.com>
2022-06-12 21:43:33 -0700

    Integrate `astro-imagetools`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://memes.yaffle.xyz/",
   trailingSlash: "always",
+  integrations: [astroImageTools],
   vite: {
     ssr: { external: ["neat-csv"] },
     optimizeDeps: { exclude: ["neat-csv"] },
