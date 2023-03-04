ed6a18aa7fe89d127ecceae086ed4c85b3a27950
Simon Wiles <simonjwiles@gmail.com>
2023-03-03 16:40:40 -0800

    Automatically generate a new Open Graph image on every build
    This took some fiddling to get right, and still depends on an arbitrary wait to allow the page to render completely, so will want some proper monitoring when running in the GitHub Actions CD environment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import { astroImageTools } from "astro-imagetools";
 
+import { memewallOgImage } from "./integrations/memewall-og-image.ts";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://memes.sucho.org/",
   trailingSlash: "always",
-  integrations: [astroImageTools, svelte()],
+  integrations: [astroImageTools, svelte(), memewallOgImage()],
   vite: {
     ssr: { external: ["neat-csv"] },
     optimizeDeps: { exclude: ["neat-csv"] },
