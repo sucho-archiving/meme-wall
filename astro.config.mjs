08d892020c07256b240b6155cd084c290d2792d5
Simon Wiles <simonjwiles@gmail.com>
2023-04-20 13:26:05 -0700

    Reorganize OpenGraph image generation
    The Astro integration approach has consistently failed in the CD environment.  Hopefully this will be more robust.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import { astroImageTools } from "astro-imagetools";
 
-import { memewallOgImage } from "./integrations/memewall-og-image.ts";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://memes.sucho.org/",
   trailingSlash: "always",
-  integrations: [astroImageTools, svelte(), memewallOgImage()],
+  integrations: [astroImageTools, svelte()],
   vite: {
     ssr: { external: ["neat-csv"] },
     optimizeDeps: { exclude: ["neat-csv"] },
---
+++ bin/create-og-image.mjs
@@ -0,0 +1,54 @@
+import { spawn } from "child_process";
+
+import { pEventIterator } from "p-event";
+import { chromium } from "playwright";
+
+
+const delay = (milliseconds) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+
+
+const astroServerProcess = spawn("pnpm", ["dev"]);
+let astroServerUrl;
+
+const asyncIterator = pEventIterator(astroServerProcess.stdout, "data");
+for await (const chunk of asyncIterator) {
+  // wait for the Astro dev server to be ready and to report its URL
+  if (
+    (astroServerUrl = chunk
+      .toString()
+      .match(/http:\/\/[\w\d]+:\d+\//)?.[0])
+  ) {
+    break;
+  }
+}
+
+if (!astroServerUrl) {
+  throw new Error("Server exited without reporting a URL!");
+}
+
+console.log(` --> Spawned Astro server listening at ${astroServerUrl}`);
+
+const browser = await chromium.launch({
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-web-security",
+  ],
+});
+
+const page = await browser.newPage();
+page.setDefaultTimeout(0);
+await page.setViewportSize({ width: 1200, height: 630 });
+await page.goto(astroServerUrl, { waitUntil: "networkidle" });
+
+// `waitUntil: "networkidle"` seems to be inadequate
+console.log(` --> Waiting for the page to render before capturing...`);
+await delay(1000);
+
+await page.screenshot({ path: `./assets/open-graph.jpeg` });
+
+await browser.close();
+astroServerProcess.kill();
