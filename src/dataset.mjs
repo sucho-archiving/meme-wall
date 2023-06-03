25bea18e2f673864ed269cb7de3f1968b5da8421
Simon Wiles <simonjwiles@gmail.com>
2023-06-02 19:25:41 -0700

    Refactor to generate derivative images when building the dataset
    This causes vite to generate a spurious `Top-level await is not available in the configured target environment` error, and the only work-around I've found so far is to exclude `astro-imagetools` from `vite.optimizeDeps`.  This is unfortunate, as it seems to cause a large increase in start-up time.  Perhaps there's another solution.

--- src/dataset.mjs
+++ src/dataset.mjs
@@ -1,6 +1,7 @@
 import path from "path";
 
 import log from "loglevel";
+import { renderPicture } from "astro-imagetools/api";
 import sizeOf from "image-size";
 
 import { memeMediaFolder } from "./config.mjs";
@@ -16,6 +17,31 @@ log.setLevel(log.levels[process.env.LOG_LEVEL || "INFO"]);
 const getAspectRatio = (imgPath) => {
   const dimensions = sizeOf(imgPath);
   return dimensions.width / dimensions.height;
+};
+
+// Given a <picture/> element as an HTML string (as returned by renderPicture),
+// extract the srcset attribute values and return an object of the form
+// { <ext>: <srcset>, ... }
+const getSrcsetFromPicture = (picture) =>
+  Object.fromEntries(
+    [...picture.matchAll(/srcset="(?<s>[^"]*?)"/g)].map((s) => [
+      s[1].match(/(?<=\.)[^\s.]+(?=\s|$)/)[0],
+      s.groups.s,
+    ]),
+  );
+
+const generateImages = async (meme) => {
+  const { picture } = await renderPicture({
+    src: "/src/../meme_media/" + meme.filename,
+    format: "webp",
+    fallbackFormat: false,
+    includeSourceFormat: false,
+    alt: meme.title,
+    placeholder: "none",
+    includeSourceFormat: false,
+  });
+  const srcsets = getSrcsetFromPicture(picture);
+  return srcsets;
 };
 
 // fetch minimally-parsed data from the spreadsheet(s)
@@ -72,6 +98,7 @@ memes = memes.filter((meme) =>
 for (const meme of memes) {
   const filepath = path.join(memeMediaFolder, meme.filename);
   meme.aspectRatio = getAspectRatio(filepath);
+  meme.srcSets = await generateImages(meme);
 }
 
 // Prepare facets and facet counts
