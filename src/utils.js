caacc070cbdd66aa25e46a385df57a9930c498d2
Simon Wiles <simonjwiles@gmail.com>
2023-08-24 08:37:37 -0700

    Strip more characters from glossary slugs

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ export const normalizeText = (text) => text.trim().replace(/\s+/g, " ");
 export const sluggify = (text) => {
   return text
     .toLowerCase()
-    .replace(/[“”"',?]/g, "")
+    .replace(/[“”"'‘’.,?()\[\]!]/g, "")
     .replace(/[\/]/g, "_")
     .replace(/\s/g, "-")
     .replace(/–/g, "-")
